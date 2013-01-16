2cf0e9007ed0d5949c950ef2fb5104cc8df48598
mimosinnet <mimosinnet@gmail.com>
2013-01-16 18:10:36 +0100

    Rewritte Batteries.pm in a moose syntax
    Rewritte Batteries.pm following a moose syntax

--- lib/Acpi/Battery/Batteries.pm
+++ lib/Acpi/Battery/Batteries.pm
@@ -15,74 +15,95 @@
 #     REVISION: ---
 #===============================================================================
 
+package Acpi::Battery::Batteries;
 use 5.010;
+use Moose;
+use namespace::autoclean;
 
-# Get the variables that will be used by the builder subrutines. {{{
-# No sure if this is the right approach
+# Define Attributes {{{
+has dir => (
+	is => 'ro',
+	isa => 'Str',
+	default => '/sys/class/power_supply',
+);
 
-my $dir = "/sys/class/power_supply";
-opendir(my $battery_dir, $dir) or die "Cannot open $dir : $!";
-my (@bats, $adaptor, $nb, $na);
-while(readdir($battery_dir))
-{
-	if ($_ =~ /BAT/)
-	{
-		push @bats, $_;
-		$nb++;
-	} 
-	elsif ($_ =~ /A.*/)
-	{
-		$adaptor = $_;
-		$na++;
-	}
-	
-}
-closedir($battery_dir);
+has batteries => (
+	is => 'ro',
+	isa => 'ArrayRef[Str]',
+	lazy => 1,
+	builder => '_batteries',
+);
 
-die "No batteries found" unless ($nb > 0);
-die "
-		The number of AC adaptors should be one, and more or none have been found. 
-		Please, fill a bug with the contents of the $dir directory
-	"
-	unless $na == 1; 
+has adaptor => (
+	is => 'ro',
+	isa => 'Str',
+	lazy => 1,
+	builder => '_adaptor',
+);
 
-my $online_file = "/sys/class/power_supply/$adaptor/online";
-my $online = do
-{
-	local @ARGV = $online_file;
-	local $/ = <ARGV>;
-};
-chomp $online;
+has on_line => (
+	is => 'ro',
+	isa => 'Bool',
+	lazy => 1,
+	builder => '_online',
+);
 #}}}
 
-{ # Define Moose Attributes {{{
-	package Acpi::Battery::Batteries;
-	use Moose;
+# Define builders {{{
+sub _batteries 
+{
+	my $self = shift;
+	my $dir = $self->dir;
 
-	has batteries => (
-		is => 'ro',
-		isa => 'ArrayRef[Str]',
-		default => sub { \@bats },
-	);
-
-	has adaptor => (
-		is => 'ro',
-		isa => 'Str',
-		default => $adaptor,
-	);
-
-	has on_line => (
-		is => 'ro',
-		isa => 'Bool',
-		default => $online,
-	);
-
-
-	sub _online 
+	opendir(my $power_supply_dir, $dir) or die "Cannot open $dir : $!";
+	my @bats;
+	while(readdir($power_supply_dir))
 	{
-		my $self = shift;
-		return 1;
+		push @bats, $_ if ($_ =~ /BAT/);
 	}
-}#}}}
+	closedir($power_supply_dir);
+
+	die "No batteries found" unless (scalar @bats > 0);
+	return \@bats;
+}
+
+sub _adaptor 
+{
+	my $self = shift;
+	my $dir = $self->dir;
+	my $adaptor;
+
+	opendir(my $power_supply_dir, $dir) or die "Cannot open $dir : $!";
+	while(readdir($power_supply_dir))
+	{
+		$adaptor = $_ if ($_ =~ /^A/);
+	}
+	closedir($power_supply_dir);
+	die "
+
+		There should be one AC adaptor. 
+		Please, fill a bug with the contents of the $dir directory
+
+	" unless defined $adaptor; 
+	say $adaptor;
+	return $adaptor;
+}
+
+sub _online 
+{
+	my $self = shift;
+	my $adaptor = $self->adaptor;
+	my $online_file = "/sys/class/power_supply/$adaptor/online";
+	my $online = do
+	{
+		local @ARGV = $online_file;
+		local $/ = <ARGV>;
+	};
+	chomp $online;
+	return $online;
+}
+#}}}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
